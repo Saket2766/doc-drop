440ea5576c94feec2f5c20d763e7d61dce342035
Saket Samarth <saket.samarth@eagleview.com>
2026-01-24 16:56:41 +0530

    basic file selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { Navbar } from "./components/navbar";
+import { UploadView } from "./components/uploadView";
+
 
 export function App() {
   return (
     <>
       <Navbar />
+      <div className="w-1/5 mx-auto">
+        <UploadView />
+      </div>
+      
     </>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,7 @@ import {NavigationMenu} from "@/components/ui/navigation-menu"
 
 export const Navbar = () => {
     return (
-        <nav className="flex p-4 mx-auto w-fit">
+        <nav className="flex py-6 px-16 mx-auto mt-12 w-fit border border-gray-200 rounded-full">
             <div>
                 <h1 className="scroll-m-20 text-center text-blue-500 text-4xl font-extrabold tracking-tight text-balance">
                     DocDrop
